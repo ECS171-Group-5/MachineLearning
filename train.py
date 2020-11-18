dfd3a44599faa3b14fe19b9f565eed7eb2f46161
Orli Feuchtwang <orli.feuchtwang@gmail.com>
2020-11-18 15:20:33 -0800

    fixed bugs, renamed variables, changed input format

--- train.py
+++ train.py
@@ -4,39 +4,34 @@ import pickle
 import pandas as pd
 import sys
 
-if len(sys.argv) != 2:
-	sys.exit("Error: incorrect number of arguments supplied. Usage: python train.py example.csv ['list','of','features','optional']")
+if len(sys.argv) < 2:
+	sys.exit("Error: incorrect number of arguments supplied. Usage: python train.py example.csv optional_feature_1 optional_feature_2...")
 
-#default features
-#NOTE: removed 'increase', if allowing user to enter features we don't want them to add the target feature
-names = ['book_value', 'market_cap', 'dividend_yield', 'eps', 'price_earnings_ratio', 'price_book_ratio','dps','current_ratio','quick_ratio']
+filepath = sys.argv[1]
 
-#checking for input of desired features
-if len(sys.argv) == 3:
-  temp = sys.argv[2]
-  temp = [x.lower() for x in temp]
-  #check if provided features exist
-  result = all(features in names for features in temp)
-  if not result:
-    sys.exit("Error: incorrect feature/s supplied. Default available features are ['book_value', 'market_cap', 'dividend_yield', 'eps', 'price_earnings_ratio', 'price_book_ratio','dps','current_ratio','quick_ratio']")
-  else :
-    names = temp
-
-#need to create dataframe with the provided features
+# load data from csv
 try:
-  dataframe = pd.read_csv(filepath, header=0) #names=names
+  dataframe = pd.read_csv(filepath)
 except:
   sys.exit("Error: csv file not found.")
 
-target = dataframe['increase'].copy() # for now 'increase' is our default target feature
+# default features
+features = ['bookValue','marketCap','dividendYield','peRatio','pbRatio','currentRatio','quickRatio','stockPrice','eps']
 
-features = dataframe[dataframe.columns[names]].copy()
+# check for custom features
+if len(sys.argv) >= 3:
+  temp = sys.argv[2:]
+  #check if provided features exist
+  result = all(feature in features for feature in temp)
+  if not result:
+    sys.exit("Error: incorrect feature(s) supplied. Default available features are {}".format(features))
+  else :
+    features = temp
 
-#split DF and train
-X_train, X_test, Y_train, Y_test = model_selection.train_test_split(features, target,test_size=.7)
+# test and train split
+X_train, X_test, Y_train, Y_test = model_selection.train_test_split(dataframe[features], dataframe['increase'],test_size=.7)
 
 model = LogisticRegression(solver = 'lbfgs')#, multi_class='multinomial'
 model.fit(X_train, Y_train)
 
-filename = 'model.sav'
-pickle.dump(model, open(filename, 'wb'))
+pickle.dump(model, open('model.sav', 'wb'))
